cbb30574d5ce08c404a0e890212560b36128d8b9
ayazmister <ayaz@mistersprint.com>
2019-12-25 18:57:27 -0600

    history deleted

--- .history/apache-tmp/apache_20191225172428.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo rm -rf /var/www/html/*.html
-
-sudo touch /var/www/html/index.html
-
-sudo chmod 777 /var/www/html/index.html
-
-echo "<h1>this is from packer</h1>" > /var/www/html/index.html
--- .history/nginx-tmp/nginx_20191225170043.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update -y
-sudo apt-get install nginx -y
-sudo service nginx start
-sudo chkconfig on
-
